d4921588ee83c8ad575700da14b8793dd23bf09f
Jimmy Lee <jimmylee88@users.noreply.github.com>
2025-12-10 21:37:32 +0000

    Initial NavBar styling with Tailwind

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,18 +3,18 @@ import { Link } from "react-router-dom";
 const NavBar = () => {
   return (
     <div className="shadow-lg">
-      <div className="flex justify-center p-2">
-        <img src="public/jl-bird.gif" width="50px" />
+      <div className="flex justify-center pt-4 pb-2">
+        <img src="public/jl-bird-dark.gif" width="60px" />
       </div>
-      <nav className="flex justify-center pt-2 px-8 pb-4">
-        <ul className="nav flex flex-row gap-8 sticky">
-          <li>
+      <nav className="flex justify-center ">
+        <ul className="nav flex flex-row gap-10 sticky font-montserrat font-bold text-xl">
+          <li className="py-2 border-b-4 border-transparent hover:border-b-indigo-800 transition-colors">
             <Link to="/">Home</Link>
           </li>
-          <li>
+          <li className="py-2 border-b-4 border-transparent hover:border-b-indigo-800 transition-colors">
             <Link to="/about">About</Link>
           </li>
-          <li>
+          <li className="py-2 border-b-4 border-transparent hover:border-b-indigo-800 transition-colors">
             <Link to="/contact">Contact</Link>
           </li>
         </ul>
